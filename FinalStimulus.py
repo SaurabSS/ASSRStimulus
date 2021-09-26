b17c5518282dcf0f458cc0a6eb119f1bb015e6ed
SaurabSS <90489188+SaurabSS@users.noreply.github.com>
2021-09-26 15:20:37 -0400

    Updated Wave mixing and Random Selection

--- FinalStimulus.py
+++ FinalStimulus.py
@@ -4,10 +4,11 @@ from psychopy import visual
 #from pydub import AudioSegment
 #from pydub.playback import play
 import random
+from pydub import AudioSegment
 
 print('Using %s (with %s) for sounds' % (sound.audioLib, sound.audioDriver))
 
-expInfo = {'Observer':'Your Name', 'Volunteer Name':'Burdell', 'Pure Tone Duration (s)' : 6, 'Dual Tone Duration (s)':10,'Volume (0-100)':25}
+expInfo = {'Observer':'Your Name', 'Volunteer Name':'Burdell', 'Pure Tone Duration (s)' : 3, 'Dual Tone Duration (s)':6,'Volume (0-100)':25}
 
 dlg = gui.DlgFromDict(expInfo, title='ASSR')
 if dlg.OK:
@@ -23,13 +24,49 @@ circle.draw()
 win.flip()
 
 for i in range(0,5):
+    sound1 = AudioSegment.from_file("a4-40.wav")
+    sound2 = AudioSegment.from_file("b5-35.wav")
+    sound3 = AudioSegment.from_file("cs6-75.wav")
+    sound4 = AudioSegment.from_file("f5-17.wav")
+    sound5 = AudioSegment.from_file("g6-23.wav")
+
+    soundFile1 = "a4-40.wav"
+    soundFile2 = "b5-35.wav"
+    soundFile3 = "cs6-75.wav"
+    soundFile4 = "f5-17.wav"
+    soundFile5 = "g6-23.wav"
+
+    
+    listSoundWavs = [sound1, sound2, sound3, sound4, sound5]
+    listSoundFiles= [soundFile1, soundFile2, soundFile3, soundFile4, soundFile5]
+    listIndices = range(1,5)
+    pick1 = random.choice(listIndices)
+    pick2 = random.choice(listIndices)
+    
+    fSound1 = listSoundWavs[pick1]
+    fSound2 = listSoundWavs[pick2]
+    fSoundFile1 = listSoundFiles[pick1]
+    fSoundFile1 = listSoundFiles[pick2]
+    
+    
+    while(abs(pick2-pick1)<2):
+        pick2 = random.choice(listIndices)
+    
+    if pick1<pick2:
+        lowerInd = sound1 
+    else:
+        lowerInd = sound2
+    
+    combined = sound1.overlay(sound2)
+    combined.export("combined.wav", format='wav')
+        
     message = visual.TextStim(win, pos=[0,+3],text='This is the sound you need to focus on.')
     message.draw()
     circle = visual.Circle(win=win, radius=1, pos=[0,0], fillColor=[1.0,1.0,1.0], colorSpace='rgb')
     circle.draw()
     win.flip()
 
-    hello = sound.Sound('b5-35.wav', secs=expInfo['Pure Tone Duration (s)']) if(i%2) else sound.Sound('cs6-75.wav')
+    hello = sound.Sound(fSoundFile1, stopTime=expInfo['Pure Tone Duration (s)']) 
     hello.play()
     core.wait(expInfo['Pure Tone Duration (s)'])
 
@@ -37,19 +74,22 @@ for i in range(0,5):
     message.draw()
     win.flip()
     event.waitKeys()
+
     circle = visual.Circle(win=win, radius=1, pos=[0,0], fillColor=[1.0,1.0,1.0], colorSpace='rgb')
     circle.draw()
     win.flip()
-    
-    hello = sound.Sound('mix12.wav') if(i%2) else sound.Sound('mix13.wav')
-    hello.setVolume(2)
+
+    hello = sound.Sound('combined.wav', stopTime=expInfo['Dual Tone Duration (s)']) 
     hello.play()
     core.wait(expInfo['Dual Tone Duration (s)'])
 
-    message = visual.TextStim(win, pos=[0,+3],text='''Could you focus on it? Press '\Y\' or \'N\' ''')
+
+    message = visual.TextStim(win, pos=[0,+3],text='''If you want to skip the last trial, press spacebar in 3 seconds''')
     message.draw()
     win.flip()
-    event.waitKeys()
+    while(core.wait(3)):
+        event.waitKeys()
+
 
 if sys.platform == 'win32':
     ding = sound.Sound('ding')
