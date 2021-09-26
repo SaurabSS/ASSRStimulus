8a2113bcd8884f0478f5f539a5901a5d52d6e6c8
SaurabSS <90489188+SaurabSS@users.noreply.github.com>
2021-09-26 19:26:37 -0400

    Capturing skipped trials in a CSV

--- FinalStimulus.py
+++ FinalStimulus.py
@@ -1,20 +1,25 @@
 import sys
 from psychopy import sound, core, event, gui
-from psychopy import visual
-#from pydub import AudioSegment
-#from pydub.playback import play
+from psychopy import visual, data
 import random
 from pydub import AudioSegment
+import csv
 
 print('Using %s (with %s) for sounds' % (sound.audioLib, sound.audioDriver))
 
-expInfo = {'Observer':'Your Name', 'Volunteer Name':'Burdell', 'Pure Tone Duration (s)' : 3, 'Dual Tone Duration (s)':6,'Volume (0-100)':25}
+expInfo = {'Observer':'Lab243', 'Volunteer Name':'Burdell', 'Pure Tone Duration (s)' : 3, 'Dual Tone Duration (s)':5,'Volume (0-100)':25, 'Total Trials':2}
 
 dlg = gui.DlgFromDict(expInfo, title='ASSR')
 if dlg.OK:
     print("continuing to the Experiment")
 else:
     core.quit()
+
+# making a csv file to save skipped trails and their timestamps
+fileName = expInfo['Volunteer Name'] + data.getDateStr()
+dataFile = open(fileName+'.csv', 'w')
+writer = csv.writer(dataFile)
+writer.writerow(["Skipped Trials"])
 
 win = visual.Window([1280,700],allowGUI=True,
                     monitor='testMonitor', units='deg')
@@ -23,7 +28,7 @@ circle = visual.Circle(win=win, radius=1, pos=[0,0], fillColor=[1.0,1.0,1.0], co
 circle.draw()
 win.flip()
 
-for i in range(0,5):
+for trialNo in range(0,expInfo['Total Trials']):
     sound1 = AudioSegment.from_file("a4-40.wav")
     sound2 = AudioSegment.from_file("b5-35.wav")
     sound3 = AudioSegment.from_file("cs6-75.wav")
@@ -87,20 +92,11 @@ for i in range(0,5):
     message = visual.TextStim(win, pos=[0,+3],text='''If you want to skip the last trial, press spacebar in 3 seconds''')
     message.draw()
     win.flip()
-    while(core.wait(3)):
-        event.waitKeys()
 
-
-if sys.platform == 'win32':
-    ding = sound.Sound('ding')
-    ding.play()
-
-    core.wait(1)
-
-    tada = sound.Sound('tada.wav')
-    tada.play()
-
-    core.wait(2)
-print('done')
-
+    if(event.waitKeys(maxWait=3)):  
+        writer.writerow(["skip"])
+    else:
+        writer.writerow(["good"])
+        
+dataFile.close()
 core.quit()
